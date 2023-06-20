c279a6e975f165d963f6dc0c005e655d1975f336
Leopoldo <rokohaster@gmail.com>
2023-06-20 10:49:12 +0200

    commit 2

--- examen.py
+++ examen.py
@@ -2,19 +2,30 @@ def mediana(diccionario):
     ascendente = {}
     lista = []
     i= 0
+    mediana = 0
     for valor in diccionario:
         lista.append(diccionario["valor"+str(i)])
         i = i+1
-    sorted(lista)
-    #ahora la mediana  de la lista sorted
-    if(len(lista)%2 == 0):
-        mediana = lista[len(lista)/2] + lista[(len(lista)/2)+1]
-        print(mediana)
+    ascendente = sorted(lista)
+    print(ascendente)
+    if len(ascendente) % 2 == 1:
+        mediana =  ascendente[len(ascendente) // 2]
     else:
-        mediana = lista[int(len(lista)/2)]
-        ascendente.update({"mediana":mediana})
+        mediana=  (ascendente[len(ascendente)//2 - 1] + ascendente[len(ascendente) // 2]) / 2
 
-
-    return ascendente
+    return mediana
 valores = {'valor0':5,'valor1':3,'valor2':8,'valor3': 1,'valor4':6}
+#1 3 5 6 8
 print(mediana(valores))      
+
+
+def procesar(fichero,numero):
+    lista_alumnos = []
+    with open(fichero, 'r') as file:
+        for linea in file:
+            palabras = linea.split()
+            if numero in palabras:
+                lista_alumnos.append(linea)
+    return lista_alumnos
+procesar()
+                    
